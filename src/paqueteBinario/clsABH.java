65a88161db087cb2c1764ccebe87f5a30288a8dd
venticolaleandro <venticolaleandro@gmail.com>
2016-06-22 15:56:15 -0300

    Modifique la clase clsABH donde le meti el unico metodo que pienso que se puede extender a otra clase

--- src/paqueteBinario/clsABH.java
+++ src/paqueteBinario/clsABH.java
@@ -1,5 +1,19 @@
 package paqueteBinario;
 
-public abstract class clsABH {
-
+public abstract class clsABH extends clsBinaryTree{
+	
+	public abstract boolean iguales(Object nodeA, Object nodeB);
+	public abstract boolean menor(Object nodeA, Object nodeB);
+	public abstract boolean mayor(Object nodeA, Object nodeB);	
+	
+	void meter(Object node, boolean pos){
+		clsBinaryTreeNode temp=this.root;
+		clsBinaryTreeNode nodo=(clsBinaryTreeNode)node;
+		if(pos){
+			temp.setRightChild(nodo);
+		}else{
+			temp.setLeftChild(nodo);
+		}	
+	}
+	
 }
